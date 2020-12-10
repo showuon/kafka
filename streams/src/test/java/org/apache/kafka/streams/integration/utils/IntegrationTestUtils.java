dd6c2b7d977980e84107d66c9bbb4ffb3c509b9c
Luke Chen <showuon@gmail.com>
2020-12-10 16:17:26 +0800

    try again

--- streams/src/test/java/org/apache/kafka/streams/integration/utils/IntegrationTestUtils.java
+++ streams/src/test/java/org/apache/kafka/streams/integration/utils/IntegrationTestUtils.java
@@ -1152,7 +1152,7 @@ public class IntegrationTestUtils {
             continueConsuming(consumerRecords.size(), maxMessages)) {
             totalPollTimeMs += pollIntervalMs;
             final ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(pollIntervalMs));
-            System.err.println("!!! read records: " + records.count());
+            System.err.println("!!! read records: " + records.count() + ", totalPollTimeMs is " + totalPollTimeMs);
 
             for (final ConsumerRecord<K, V> record : records) {
                 consumerRecords.add(record);
