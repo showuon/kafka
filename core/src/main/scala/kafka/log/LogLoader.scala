bba605e240e34fcd6ccc4676904cc12aa5c59caa
Luke Chen <showuon@gmail.com>
2022-04-14 15:59:03 +0800

    id

--- core/src/main/scala/kafka/log/LogLoader.scala
+++ core/src/main/scala/kafka/log/LogLoader.scala
@@ -79,7 +79,7 @@ class LogLoader(
   leaderEpochCache: Option[LeaderEpochFileCache],
   producerStateManager: ProducerStateManager
 ) extends Logging {
-  logIdent = s"[LogLoader partition=$topicPartition, dir=${dir.getParent}, thread=${Thread.currentThread().getName}] "
+  logIdent = s"[LogLoader partition=$topicPartition, dir=${dir.getParent}, thread=${Thread.currentThread().getId}] "
 
   /**
    * Load the log segments from the log files on disk, and returns the components of the loaded log.
