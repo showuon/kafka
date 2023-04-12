870bfa7ad1d8df65c2fbfa5eed246e88d34d23c9
Luke Chen <showuon@gmail.com>
2023-04-12 09:46:02 +0800

    remove unused AsyncReadTask

--- core/src/main/java/kafka/log/remote/RemoteLogManager.java
+++ core/src/main/java/kafka/log/remote/RemoteLogManager.java
@@ -805,37 +805,6 @@ public class RemoteLogManager implements Closeable {
     }
 
     /**
-     * A remote log read task returned by asyncRead(). The caller of asyncRead() can use this object to cancel a
-     * pending task or check if the task is done.
-     */
-    public class AsyncReadTask {
-
-        private final Future<Void> future;
-
-        AsyncReadTask(Future<Void> future) {
-            this.future = future;
-        }
-
-        public boolean cancel(boolean mayInterruptIfRunning) {
-            boolean cancelled = future.cancel(mayInterruptIfRunning);
-            if (cancelled) {
-                // Removed the cancelled task from task queue
-                remoteStorageReaderThreadPool.purge();
-            }
-
-            return cancelled;
-        }
-
-        public boolean isCancelled() {
-            return future.isCancelled();
-        }
-
-        public boolean isDone() {
-            return future.isDone();
-        }
-    }
-
-    /**
      * Submit a remote log read task.
      *
      * This method returns immediately. The read operation is executed in a thread pool.
