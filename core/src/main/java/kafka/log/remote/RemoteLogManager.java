221a2254f8b80e4eec1e9e92b71b49a07b2e893e
Satish Duggana <satishd@apache.org>
2023-06-20 12:29:07 +0530

    Addressed reveiw comments Updating _localLogStartOffset appropriately covering both the scenarios.

--- core/src/main/java/kafka/log/remote/RemoteLogManager.java
+++ core/src/main/java/kafka/log/remote/RemoteLogManager.java
@@ -745,9 +745,9 @@ public class RemoteLogManager implements Closeable {
                             new RemoteLogSegmentMetadataUpdate(segmentMetadata.remoteLogSegmentId(), time.milliseconds(),
                                     RemoteLogSegmentState.DELETE_SEGMENT_FINISHED, brokerId)).get();
                     return true;
-                } else {
-                    return false;
                 }
+
+                return false;
             }
 
         }
@@ -761,11 +761,13 @@ public class RemoteLogManager implements Closeable {
             // Cleanup remote log segments and update the log start offset if applicable.
             final Iterator<RemoteLogSegmentMetadata> segmentMetadataIter = remoteLogMetadataManager.listRemoteLogSegments(topicIdPartition);
             if (!segmentMetadataIter.hasNext()) {
+                logger.debug("No remote log segments available on remote storage for partition: {}", topicIdPartition);
                 return;
             }
 
             final Optional<UnifiedLog> logOptional = fetchLog.apply(topicIdPartition.topicPartition());
             if (!logOptional.isPresent()) {
+                logger.debug("No UnifiedLog instance available for partition: {}", topicIdPartition);
                 return;
             }
 
@@ -817,6 +819,7 @@ public class RemoteLogManager implements Closeable {
                 Iterator<RemoteLogSegmentMetadata> segmentsIterator = remoteLogMetadataManager.listRemoteLogSegments(topicIdPartition, epochEntry.epoch);
                 while (isSegmentDeleted && segmentsIterator.hasNext()) {
                     if (isCancelled() || !isLeader()) {
+                        logger.info("Returning from remote log segments cleanup for the remaining segments as the task state is changed.");
                         return;
                     }
 
@@ -1199,6 +1202,10 @@ public class RemoteLogManager implements Closeable {
         private final long remainingBreachedSize;
 
         public RetentionSizeData(long retentionSize, long remainingBreachedSize) {
+            if (retentionSize < remainingBreachedSize) {
+                throw new IllegalArgumentException("retentionSize must be greater than remainingBreachedSize");
+            }
+
             this.retentionSize = retentionSize;
             this.remainingBreachedSize = remainingBreachedSize;
         }
@@ -1210,6 +1217,10 @@ public class RemoteLogManager implements Closeable {
         private final long cleanupUntilMs;
 
         public RetentionTimeData(long retentionMs, long cleanupUntilMs) {
+            if (retentionMs < cleanupUntilMs) {
+                throw new IllegalArgumentException("retentionMs must be greater than cleanupUntilMs");
+            }
+
             this.retentionMs = retentionMs;
             this.cleanupUntilMs = cleanupUntilMs;
         }
