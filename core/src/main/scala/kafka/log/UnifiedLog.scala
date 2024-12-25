b539ced3042ec234bd871797afd990865583fd57
Luke Chen <showuon@gmail.com>
2024-12-25 20:01:40 +0800

    roll segment

--- core/src/main/scala/kafka/log/UnifiedLog.scala
+++ core/src/main/scala/kafka/log/UnifiedLog.scala
@@ -1573,7 +1573,7 @@ class UnifiedLog(@volatile var logStartOffset: Long,
     val maxOffsetInMessages = appendInfo.lastOffset
 
     if (segment.shouldRoll(new RollParams(config.maxSegmentMs, config.segmentSize, appendInfo.maxTimestamp, appendInfo.lastOffset, messagesSize, now))) {
-      debug(s"Rolling new log segment (log_size = ${segment.size}/${config.segmentSize}}, " +
+      info(s"Rolling new log segment (log_size = ${segment.size}/${config.segmentSize}}, " +
         s"offset_index_size = ${segment.offsetIndex.entries}/${segment.offsetIndex.maxEntries}, " +
         s"time_index_size = ${segment.timeIndex.entries}/${segment.timeIndex.maxEntries}, " +
         s"inactive_time_ms = ${segment.timeWaitedForRoll(now, maxTimestampInMessages)}/${config.segmentMs - segment.rollJitterMs}).")
