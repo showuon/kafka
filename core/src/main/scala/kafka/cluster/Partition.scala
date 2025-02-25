a6f1330b08772a022a102e1d0a018ef132db717a
Luke Chen <showuon@gmail.com>
2025-02-25 12:37:20 +0800

    recoveri log works

--- core/src/main/scala/kafka/cluster/Partition.scala
+++ core/src/main/scala/kafka/cluster/Partition.scala
@@ -488,7 +488,7 @@ class Partition(val topicPartition: TopicPartition,
         0L
       })
       val initialHighWatermark = log.updateHighWatermark(checkpointHighWatermark)
-      info(s"Log loaded for partition $topicPartition with initial high watermark $initialHighWatermark")
+      info(s"Log loaded for partition $topicPartition with initial high watermark $initialHighWatermark;; $offsetCheckpoints.")
     }
 
     logManager.initializingLog(topicPartition)
