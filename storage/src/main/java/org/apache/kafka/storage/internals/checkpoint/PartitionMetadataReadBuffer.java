89ccb812ea714bd5edda5c58021ebc9d0d3453b5
Luke Chen <showuon@gmail.com>
2025-03-05 14:05:15 +0800

    partitionMetadata

--- storage/src/main/java/org/apache/kafka/storage/internals/checkpoint/PartitionMetadataReadBuffer.java
+++ storage/src/main/java/org/apache/kafka/storage/internals/checkpoint/PartitionMetadataReadBuffer.java
@@ -18,38 +18,50 @@
 package org.apache.kafka.storage.internals.checkpoint;
 
 import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.storage.StorageManager;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class PartitionMetadataReadBuffer {
     private static final Pattern WHITE_SPACES_PATTERN = Pattern.compile(":\\s+");
 
     private final String location;
-    private final BufferedReader reader;
+    private final StorageManager storageManager;
 
     public PartitionMetadataReadBuffer(
         String location,
-        BufferedReader reader
+        StorageManager storageManager
     ) {
         this.location = location;
-        this.reader = reader;
+        this.storageManager = storageManager;
     }
 
     PartitionMetadata read() throws IOException {
         String line = null;
         Uuid metadataTopicId;
+        ByteBuffer buffer = ByteBuffer.allocate((int) storageManager.size(location, StorageManager.StorageType.METADATA));
+        storageManager.read(location, buffer, 0, StorageManager.StorageType.METADATA);
+        buffer.flip();
+        String content = StandardCharsets.UTF_8.decode(buffer).toString();
+        List<String> lines = content.lines().toList();
+        if (lines.size() != 2) {
+            throw malformedLineException(content);
+        }
 
         try {
-            line = reader.readLine();
+            line = lines.get(0);
             String[] versionArr = WHITE_SPACES_PATTERN.split(line);
 
             if (versionArr.length == 2) {
                 int version = Integer.parseInt(versionArr[1]);
                 // To ensure downgrade compatibility, check if version is at least 0
                 if (version >= PartitionMetadataFile.CURRENT_VERSION) {
-                    line = reader.readLine();
+                    line = lines.get(1);
                     String[] topicIdArr = WHITE_SPACES_PATTERN.split(line);
 
                     if (topicIdArr.length == 2) {
