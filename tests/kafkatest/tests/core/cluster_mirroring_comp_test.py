c0a92602849fed72c981711f4dbec3e9e25d092e
Luke Chen <showuon@gmail.com>
2026-05-28 18:47:18 +0900

    validate

--- tests/kafkatest/tests/core/cluster_mirroring_comp_test.py
+++ tests/kafkatest/tests/core/cluster_mirroring_comp_test.py
@@ -198,6 +198,7 @@ class ClusterMirroringCompPlainTest(MirrorUtils, Test):
                                   err_msg="Mirror did not catch up after broker 0 stopped")
         time.sleep(5)
         log_hashes("after source broker 0 stopped (broker 0 should be out of sync)")
+        self.wait_for_log_convergence(self.dest_kafka, self.source_kafka, self.topics)
 
         self.logger.info("ULE 1: stop broker 1, start broker 0 (stale), elect it as leader")
         self.source_kafka.stop_node(src_broker1)
@@ -211,6 +212,7 @@ class ClusterMirroringCompPlainTest(MirrorUtils, Test):
                                   err_msg="Mirror did not catch up after ULE 1")
         time.sleep(5)
         log_hashes("after ULE 1 (broker 1 should be out of sync)")
+        self.wait_for_log_convergence(self.dest_kafka, self.source_kafka, self.topics)
 
         self.logger.info("Start broker 1, and wait for all logs are converged")
         self.source_kafka.start_node(src_broker1)
