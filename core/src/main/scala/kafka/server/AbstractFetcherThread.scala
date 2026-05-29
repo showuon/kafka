68f7ca875f2f89c41c7d339bc9ec64cd485c6632
Luke Chen <showuon@gmail.com>
2026-05-29 17:53:46 +0900

    no mirror

--- core/src/main/scala/kafka/server/AbstractFetcherThread.scala
+++ core/src/main/scala/kafka/server/AbstractFetcherThread.scala
@@ -707,7 +707,8 @@ abstract class AbstractFetcherThread(name: String,
     } else if (initialFetchState.initOffset < 0) {
       logger.info("!!! entering2:" + tp + ";;" + initialFetchState)
       fetchOffsetAndTruncate(tp, initialFetchState.topicId, initialFetchState.currentLeaderEpoch, initialFetchState.mirrorLeaderEpoch)
-    } else if (leader.isTruncationOnFetchSupported && mirrorName.isBlank) {
+//    } else if (leader.isTruncationOnFetchSupported && mirrorName.isBlank) {
+    } else if (leader.isTruncationOnFetchSupported) {
       logger.info("!!! entering3:" + tp + ";;" + initialFetchState)
       // With old message format, `latestEpoch` will be empty and we use Truncating state to truncate to high watermark
       val lastFetchedEpoch: Optional[Integer] = latestEpoch(tp)
