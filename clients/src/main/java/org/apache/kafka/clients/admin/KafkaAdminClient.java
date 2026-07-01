a5b375bbd3449c86790928f25639bb0c5855459b
Luke Chen <showuon@gmail.com>
2026-07-01 20:36:37 +0900

    fix unmodifiedMap error

--- clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
+++ clients/src/main/java/org/apache/kafka/clients/admin/KafkaAdminClient.java
@@ -4866,13 +4866,15 @@ public class KafkaAdminClient extends AdminClient {
         final Call call = new Call("createClusterMirror", calcDeadlineMs(now, options.timeoutMs()),
                 new LeastLoadedBrokerOrActiveKController()) {
 
+            @SuppressWarnings("unchecked")
             @Override
             CreateClusterMirrorRequest.Builder createRequest(int timeoutMs) {
                 if (!configs.containsKey(CommonClientConfigs.MIRROR_SOURCE_CLUSTER_ID_CONFIG)) {
-                    Map<String, Object> adminConfig = new HashMap<>(configs);
-                    try (Admin sourceAdmin = Admin.create(adminConfig)) {
+                    Map<String, String> mutableConfigs = new HashMap<>(configs);
+                    try (Admin sourceAdmin = Admin.create((Map) mutableConfigs)) {
                         var sourceClusterId = sourceAdmin.describeCluster().clusterId().get();
-                        configs.put(CommonClientConfigs.MIRROR_SOURCE_CLUSTER_ID_CONFIG, sourceClusterId);
+                        mutableConfigs.put(CommonClientConfigs.MIRROR_SOURCE_CLUSTER_ID_CONFIG, sourceClusterId);
+                        return new CreateClusterMirrorRequest.Builder(mirrorName, mutableConfigs);
                     } catch (Exception e) {
                         log.error("Failed to get cluster id from source cluster", e);
                         throw new RuntimeException(e);
