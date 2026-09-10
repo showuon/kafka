8238abe07d0a40a34cf8063507ff154851fe422b
Luke Chen <showuon@gmail.com>
2026-09-10 18:58:56 +0900

    support ULE in system test

--- tests/kafkatest/tests/core/cluster_mirroring_test.py
+++ tests/kafkatest/tests/core/cluster_mirroring_test.py
@@ -774,7 +774,11 @@ class ClusterMirroringTest(MirrorUtils, Test):
 
         dest_broker0 = self.dest_kafka.nodes[0]
         enable_ule_support_cmd = "%s --entity-type topics --entity-name %s --alter --add-config mirror.support.unclean.leader.election=true" % \
+<<<<<<< HEAD
                (self.dest_kafka.kafka_configs_cmd_with_optional_security_settings(dest_broker0, force_use_zk_connection=False), "my-topic")
+=======
+               (self.kafka.kafka_configs_cmd_with_optional_security_settings(dest_broker0, force_use_zk_connection=False), "my-topic")
+>>>>>>> 57f12db119 (support ULE in system test)
         dest_broker0.account.ssh(enable_ule_support_cmd)
 
         self.logger.info("Stop source broker 0 (broker 0 becomes stale)")
