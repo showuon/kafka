608d25d4fd108485ccbbf66661f06405046237a2
Luke Chen <showuon@gmail.com>
2026-09-15 17:19:16 +0900

    avoid duplicated state transition

--- core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
+++ core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
@@ -440,6 +440,7 @@ public class MirrorMetadataManager implements MetadataPublisher, AutoCloseable {
             if (mirrorName == null) {
                 return;
             }
+            mirrorCache.removeInProgressPartition(tp);
             mirrorCache.getPendingLederEpochBumps().removeIf(bump -> {
                 bump.partitionToEpoch().remove(tp);
                 if (bump.partitionToEpoch().isEmpty()) {
@@ -790,6 +791,12 @@ public class MirrorMetadataManager implements MetadataPublisher, AutoCloseable {
         coordinatorWriter.ifPresent(writer -> {
             for (TopicPartition tp : topicPartitions) {
                 MirrorPartitionState currentState = getPartitionState(mirrorName, tp);
+                MirrorPartitionState inProgressState = mirrorCache.inProgressPartition(tp);
+                if (inProgressState != null && inProgressState == state) {
+                    log.warn("Partition {} is in progress for {} state. Skipping transition from {} to {}", tp, inProgressState, currentState, state);
+                    continue;
+                }
+                mirrorCache.addInProgressPartition(tp, state);
                 if (!MirrorPartition.isValidStateTransition(currentState, state)) {
                     log.warn("Skipping invalid transition from {} to {} for partition {}", currentState, state, tp);
                     continue;
