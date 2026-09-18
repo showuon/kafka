a8a941046629fa5816adca3e4813d91f01f7dccb
Luke Chen <showuon@gmail.com>
2026-09-18 17:20:10 +0900

    don't set retryAttempt > maxAttempt value

--- core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
+++ core/src/main/java/kafka/server/mirror/MirrorMetadataManager.java
@@ -1074,8 +1074,9 @@ public class MirrorMetadataManager implements MetadataManagerBridge, MetadataPub
 
     private void updateLocalFailedState(MirrorPartitionKey key, MirrorPartitionState newState,
                                         String errorMessage, boolean nonRetryable) {
+        int maxAttempts = brokerConfig.mirrorConfig().failedRetryMaxAttempts();
         MirrorPartitionState curState = MirrorPartition.orEmpty(mirrorCache.getPartition(key)).state();
-        mirrorCache.updateFailedInfo(key, curState, newState, errorMessage, nonRetryable);
+        mirrorCache.updateFailedInfo(key, curState, newState, errorMessage, nonRetryable, maxAttempts);
     }
 
     private Map<TopicPartition, Integer> getLatestLocalEpoch(TopicPartition tp) {
@@ -1891,7 +1892,8 @@ public class MirrorMetadataManager implements MetadataManagerBridge, MetadataPub
     @Override
     public void updateFailedInfo(MirrorPartitionKey key, MirrorPartitionState currentState,
                                  MirrorPartitionState newState, String errorMessage, boolean nonRetryable) {
-        mirrorCache.updateFailedInfo(key, currentState, newState, errorMessage, nonRetryable);
+        int maxAttempts = brokerConfig.mirrorConfig().failedRetryMaxAttempts();
+        mirrorCache.updateFailedInfo(key, currentState, newState, errorMessage, nonRetryable, maxAttempts);
     }
 
     public void clearFailedInfo(String mirrorName, TopicPartition tp) {
--- core/src/main/java/kafka/server/mirror/MirrorStateCache.java
+++ core/src/main/java/kafka/server/mirror/MirrorStateCache.java
@@ -111,10 +111,10 @@ public class MirrorStateCache {
     }
 
     public void updateFailedInfo(MirrorPartitionKey key, MirrorPartitionState curState,
-                                 MirrorPartitionState newState, String errorMessage, boolean nonRetryable) {
+                                 MirrorPartitionState newState, String errorMessage, boolean nonRetryable, int maxAttempts) {
         MirrorPartition existing = MirrorPartition.orEmpty(getPartition(key));
         if (newState == MirrorPartitionState.FAILED) {
-            int attempt = existing.nextAttempt(nonRetryable);
+            int attempt = existing.nextAttempt(nonRetryable, maxAttempts);
             MirrorPartitionState previousState = existing.resolvePrevState(curState);
             partitions.compute(key, (k, e) -> MirrorPartition.orEmpty(e).withError(errorMessage, attempt, previousState));
         } else if ((curState != MirrorPartitionState.FAILED && curState != newState)
